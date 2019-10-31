5edc3f36c6d06ebed6cae2b03f9e4dfcadff1469
Bruno <bsmlima@outlook.com>
2019-10-31 15:02:56 -0300

    Find parabola without rotation

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import argparse
+from collections import defaultdict
 
 # Usage:
 # $ python main.py -i img_path
@@ -40,6 +41,9 @@ def cross_point(hough_1, hough_2):
     x = (rho1 - y * np.sin(theta1))/np.cos(theta1)
     return x, y
 
+def int_cross_point(hough_1, hough_2):
+    cross_p = cross_point(hough_1, hough_2)
+    return int(cross_p[0]), int(cross_p[1])
 
 # Main pipeline functions
 
@@ -52,8 +56,10 @@ def parse_input_img():
 
 
 def filter_image(img):
+    # Low pass filter
+    blur = cv2.blur(img,(3,3))
     # Convert to greyscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
     # Get edge map
     edges = cv2.Canny(gray, 100, 200, apertureSize=3)
     # Dilate edge map to fill holes
@@ -95,7 +101,7 @@ def get_axis(img):
 # Draw black lines on the filtered image to erase the influence of the axis
 # on further processing
 def erase_axis_points(img, first_axis, second_axis):
-    erase_width = 20
+    erase_width = 30
     draw__hough_line(img, first_axis, (0, 0, 0), erase_width)
     draw__hough_line(img, second_axis, (0, 0, 0), erase_width)
 
@@ -123,14 +129,88 @@ def draw_axis(img, first_axis, second_axis):
     draw__hough_line(img, first_axis, (255, 0, 0), axis_width)
     draw__hough_line(img, second_axis, (255, 0, 0), axis_width)
 
+def draw_parabola(img, first_axis, second_axis, xo, yo, p):
+    xorigin, yorigin = int_cross_point(first_axis, second_axis)
+    width = img.shape[1]
+
+    for x in range(-xorigin, width - xorigin):
+        try: # Catch division by 0
+            y = int(((x-xo)**2) / (4*p)) - yo
+            xpixel, ypixel = x + xorigin, y + yorigin
+            cv2.circle(img, (xpixel, ypixel), 3, (0, 255, 0), 3)
+        except:
+            pass
+
+def find_most_voted_parabola(acc, height, width):
+    highest_votes = 0
+
+    for x in range(width):
+        for y in range(height):
+            dic = acc[x][y]
+            for p, votes in dic.items():
+                if votes > highest_votes:
+                    highest_votes = votes
+                    most_voted = [x, y, p]
+
+    return most_voted
+
+def hough_parabola(img, p_precision, downscale_ratio):
+    # Downscale the image
+    img = cv2.resize(img, (0,0), fx=downscale_ratio, fy=downscale_ratio)
+    p_precision *= downscale_ratio
+
+    height, width = img.shape[:2]
+    acc = [[defaultdict(lambda: 0) for x in range(height)] for y in range(width)]
+
+    # For each white pixel on filtered image (x, y)
+    for x in range(width):
+        for y in range(height):
+            if img[y, x] != 0:
+                # For each possible parabola starting point (xo, yo)
+                for xo in range(width):
+                    for yo in range(height):
+                        try: # To catch division by 0
+                            # Calculate p for the given starting point (xo, yo)
+                            p = (x - xo)**2 / (4 * (y - yo))
+                            p = round(p / p_precision) * p_precision
+
+                            if abs(p) > 2:
+                                acc[xo][yo][p] += 1
+                        except:
+                            pass
+
+    best_parabola = find_most_voted_parabola(acc, height, width)
+
+    # Upscale parabola to fit on the original image
+    best_parabola[0] = int(best_parabola[0] / downscale_ratio)
+    best_parabola[1] = int(best_parabola[1] / downscale_ratio)
+    best_parabola[2] = best_parabola[2] / downscale_ratio
+
+    return best_parabola
+
+def get_parabola(img, first_axis, second_axis):
+    x, y, p = hough_parabola(img, 0.01, 0.2)
+    xorigin, yorigin = int_cross_point(first_axis, second_axis)
+
+    x = x - xorigin
+    y = yorigin - y
+
+    return x, y, p
+
 
 if __name__ == '__main__':
     img = parse_input_img()
     filtered = filter_image(img)
+    # Axis handling
     first_axis, second_axis = get_axis(filtered)
     erase_axis_points(filtered, first_axis, second_axis)
     adjust_axis(first_axis, second_axis)
     draw_axis(img, first_axis, second_axis)
-    cv2.imshow("Image", img)
+    # Parabola handling
+    x, y, p = get_parabola(filtered, first_axis, second_axis)
+    draw_parabola(img, first_axis, second_axis, x, y, p)
+    # Show result image
+    cv2.imshow("Result", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    
